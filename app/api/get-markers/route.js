91a20741ed793717deff27b69e48274440ccc642
albatrosdx <albatrosdx@gmail.com>
2024-06-23 21:02:45 +0900

    log追加

--- app/api/get-markers/route.js
+++ app/api/get-markers/route.js
@@ -14,6 +14,7 @@ export async function GET(req, res) {
         return res.status(500).json({ error: error.message });
       }
       // データをクライアントに返す
+      console.log(data);
       return NextResponse.json(data);
     } catch (error) {
       console.error("Unexpected error:", error);
